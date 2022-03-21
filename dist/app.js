b3b9686e545c6c043cee35c12d74a9db2fd1e68d
chei7tain <fredrickokwuobi@gmail.com>
2022-03-21 19:15:45 +0100

    update server listener

--- dist/app.js
+++ dist/app.js
@@ -15,42 +15,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fastify = require("fastify")({
     logger: true,
 });
-// fastify swagger
-fastify.register(require("fastify-swagger"), {
-    routePrefix: "/documentation",
-    swagger: {
-        info: {
-            title: "Fee API",
-            description: "Fee API",
-            version: "1.0.0",
-        },
-        externalDocs: {
-            url: "https://swagger.io",
-            description: "Find more info here",
-        },
-        host: "localhost:3000",
-        schemes: ["http"],
-        consumes: ["application/json"],
-        produces: ["application/json"],
-        tags: [
-            {
-                name: "Lanister API",
-                description: "Fee APIs",
-            },
-        ],
-    },
-});
 const fastify_cors_1 = __importDefault(require("fastify-cors"));
 fastify.register(fastify_cors_1.default);
 fastify.register(require("./routes/fastifyRoutes"));
-const port = process.env.PORT || 3000;
 const start = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        fastify.listen(process.env.PORT || 3000, process.env.HOST || "localhost", (err) => {
-            if (err)
-                throw err;
-            console.log(`server listening on ${fastify.server.address().port}`);
-        });
+        if (process.env.NODE_ENV === "production") {
+            yield fastify.listen(process.env.PORT, "0.0.0.0");
+        }
+        else {
+            fastify.listen(process.env.PORT || 3000, process.env.HOST || "localhost", (err) => {
+                if (err)
+                    throw err;
+                console.log(`server listening on ${fastify.server.address().port}`);
+            });
+        }
     }
     catch (err) {
         fastify.log.error(err);
--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,27 @@
 const fastify = require("fastify")({
   logger: true,
 });
-// fastify swagger
-fastify.register(require("fastify-swagger"), {
-  routePrefix: "/documentation",
-  swagger: {
-    info: {
-      title: "Fee API",
-      description: "Fee API",
-      version: "1.0.0",
-    },
-    externalDocs: {
-      url: "https://swagger.io",
-      description: "Find more info here",
-    },
-    host: "localhost:3000",
-    schemes: ["http"],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-    tags: [
-      {
-        name: "Lanister API",
-        description: "Fee APIs",
-      },
-    ],
-  },
-});
+
 import fastifyCors from "fastify-cors";
 
 fastify.register(fastifyCors);
 
 fastify.register(require("./routes/fastifyRoutes"));
 
-const port = process.env.PORT || 3000;
-
 const start = async () => {
   try {
-    fastify.listen(
-      process.env.PORT || 3000,
-      process.env.HOST || "localhost",
-      (err: any) => {
-        if (err) throw err;
-        console.log(`server listening on ${fastify.server.address().port}`);
-      }
-    );
+    if (process.env.NODE_ENV === "production") {
+      await fastify.listen(process.env.PORT, "0.0.0.0");
+    } else {
+      fastify.listen(
+        process.env.PORT || 3000,
+        process.env.HOST || "localhost",
+        (err: any) => {
+          if (err) throw err;
+          console.log(`server listening on ${fastify.server.address().port}`);
+        }
+      );
+    }
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
